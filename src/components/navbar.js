12596d1562eb5353d4d76ed46b38bc1fe43c091e
shawnelldh <54282973+shawnelldh@users.noreply.github.com>
2022-11-13 16:17:07 -0500

    1 file modified

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,26 @@
 import {Button, Container, Navbar, Modal} from 'react-bootstrap'
+import {useState, useContext}. from 'react';
+import {CartContext} from "../CartContext";
+import CartProduct from './CartProduct';                        }
 
 function NavbarComponent() {
+   const cart = useContext{CartContext};
+
+   const [show, setShow] = useState(false);
+   const  handleClose = () =>setShow(false);
+   const handleShow = () =>setShow(true);
+
+   const checkout = async () => {
+      const cart = useContext(CartContext)
+      
+ 
+
+
+
+
+
+
+   }
 
  return (
     <Navbar expand="sm">
